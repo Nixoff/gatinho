7d81e00640da3276e821a24208c212075e0db782
Gabriel M. Teixeira <gabadev48@protonmail.com>
2022-09-04 20:42:43 -0300

    base url no env

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  url = `http://localhost:3000/user/login`;
+  url = `${environment.API_URL}/user/login`;
+
   constructor(private http: HttpClient) {}
 
   autenticar(usuario: string, senha: string): Observable<any> {
