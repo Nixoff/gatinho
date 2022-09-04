16d7fe6f85fbde7f5ec334a8c14570313cb1b45a
Gabriel M. Teixeira <gabadev48@protonmail.com>
2022-09-04 20:01:58 -0300

    ajuste autenticação usuario

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,26 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe((res) => {
+      console.log('Autenticado com sucesso ;)');
+      console.log(res.email);
+    }, (error) => {
+      alert('Usuário ou senha inválido')
+      console.log(error);
+    })
   }
 
+  ngOnInit(): void {}
 }
