824a75fde861024b32156c106b899748a78154d4
Gabriel M. Teixeira <gabadev48@protonmail.com>
2022-09-05 20:11:26 -0300

    manipula verificação de tamanho da tela

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { media } from '../componentes/media';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  alignItems!: string;
+  range = `(min-width: 768px) and (max-width: 991px)`;
+  range$ = media(this.range);
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    media(this.range).subscribe((matches) => {
+      matches ? (this.alignItems = 'center') : (this.alignItems = 'center');
+    });
   }
-
 }
