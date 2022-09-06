f5f8c1d4e63fdbbeda91a9a72ec66881a9c3cace
Gabriel M. Teixeira <gabadev48@protonmail.com>
2022-09-05 22:07:04 -0300

    ajuste verificação de usuario existente

--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -7,11 +7,15 @@ import { NovoUsuario } from './novo-usuario';
   providedIn: 'root',
 })
 export class NovoUsuarioService {
-  url = `${environment.API_URL}/user/signup`;
+  url = `${environment.API_URL}/user`;
 
   constructor(private http: HttpClient) {}
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.http.post(this.url, novoUsuario);
+    return this.http.post(`${this.url}/signup`, novoUsuario);
+  }
+
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.http.get(`${this.url}/exists/${nomeUsuario}`);
   }
 }
